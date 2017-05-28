681c47ee33f8fe541a70499e90be783a1a59fdb2
Dilip Pareba <dilippareba@gmail.com>
2017-05-28 16:45:43 +0530

    updated check after login according to user type

--- resources/views/clinics/newuser.blade.php
+++ resources/views/clinics/newuser.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('title')
-	| First Time User
+| First Time User
 @stop
 @section('content')
 <div class="container">
@@ -9,17 +9,23 @@
             <div class="panel panel-default">
                 <div class="panel-heading">First Time User ?</div>
                 <div class="panel-body">
-                     <div class="row">
-                     	<div class="col-md-6">
-                     		<a href="{{route('clinics.create')}}" class="btn btn-success btn-block">Create a New Clinic</a>
-                     	</div>
-                     	<div class="col-md-6">
-                     		<a class="btn btn-warning btn-block" href="{{route('clinicinput')}}">Register with Existing Clinic</a>
-                     	</div>
-                     </div>                  
-                </div>
+                   <div class="row">
+                      @if (Auth::user()->doctype == "DOCTOR")
+                      <div class="col-md-6">
+                        <a href="{{route('clinics.create')}}" class="btn btn-success btn-block">Create a New Clinic</a>
+                    </div>
+                    <div class="col-md-6">
+                        <a class="btn btn-warning btn-block" href="{{route('clinicinput')}}">Register with Existing Clinic</a>
+                    </div>
+                    @else
+                    <div class="col-md-8 col-md-offset-2">
+                        <a class="btn btn-warning btn-block" href="{{route('clinicinput')}}">Register with Existing Clinic</a>
+                    </div>
+                    @endif
+                </div>                  
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
